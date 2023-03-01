cd30e1ecde75ee928c1b8cf74941350a44188580
sirpuffin1 <angelcruz981@yahoo.com>
2023-02-28 16:12:38 -0800

    feat(topics): create get all query

--- src/server/api/routers/topics.ts
+++ src/server/api/routers/topics.ts
@@ -1,10 +1,14 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const topicRouter = createTRPCRouter({
-
-})
+  getAll: protectedProcedure
+    .query(({ ctx }) => {
+      return ctx.prisma.topic.findMany({
+        where: {
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+});
