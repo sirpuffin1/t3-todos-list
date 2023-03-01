cb2bbccc050c4be58fde7489c973853fdaa095bd
sirpuffin1 <angelcruz981@yahoo.com>
2023-03-01 15:52:54 -0800

    feat(components): create note editor

---
+++ src/components/NoteEditor.tsx
@@ -0,0 +1,13 @@
+import CodeMirror from "@uiw/react-codemirror"
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+
+const NoteEditor = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default NoteEditor;
