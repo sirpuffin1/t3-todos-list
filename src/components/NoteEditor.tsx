92b7144fb542ea0f99de59cca1ee985a285b5a97
sirpuffin1 <angelcruz981@yahoo.com>
2023-03-01 16:00:23 -0800

    feat(note): configure editor

--- src/components/NoteEditor.tsx
+++ src/components/NoteEditor.tsx
@@ -1,13 +1,31 @@
-import CodeMirror from "@uiw/react-codemirror"
+import CodeMirror from "@uiw/react-codemirror";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
+import { useState } from "react";
 
-const NoteEditor = () => {
-    return (
-        <div>
-            Enter
-        </div>
-    );
-}
+const NoteEditor = ({
+  onSave,
+}: {
+  onSave: (note: { title: string; content: string }) => void;
+}) => {
+  const [code, setCode] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+
+  return (
+    <div className="card mt-5 border border-gray-200 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">
+          <input
+            type="text"
+            placeholder="Note Title"
+            className="input-primary input input-lg w-full font-bold"
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}
+          />
+        </h2>
+      </div>
+    </div>
+  );
+};
 
 export default NoteEditor;
